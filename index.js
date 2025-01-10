416b7d5e0a9ef5750f883c6d649ed576a36f2fbd
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-10 22:24:26 +0600

    part 7 complete

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function run() {
     const menuCollection = client.db("clone_bistroDb").collection("menu");
     const reviewCollection = client.db("clone_bistroDb").collection("reviews");
     const cartCollection = client.db("clone_bistroDb").collection("carts");
+    const paymentCollection = client.db("clone_bistroDb").collection("payments");
 
     // jwt related api
     app.post('/jwt', async (req, res) => {
@@ -214,6 +215,53 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret  
       })
+    });
+
+    //payment related api
+    // app.post('/payments', async(req, res) =>{
+    //   const payment = req.body;
+    //   const paymentResult = await paymentCollection.insertOne(payment);
+
+    //   //carefully delete each item from the database
+    //   console.log('payment info', payment);
+
+    //   const query = { _id: {
+    //     $in: payment.cardIds.map(id => new ObjectId(id))
+    //   }};
+
+    //   const deleteResult = await cartCollection.deleteMany(query);
+    //   res.send({paymentResult, deleteResult});
+    // })
+
+    //----------------
+
+    //get payments history
+    app.get('/payments/:email', verifyToken, async(req, res) =>{
+      const query = { email: req.params.email};
+
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({ message: 'forbidden access'});
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    //payments post api
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //  carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
     })
 
     // Send a ping to confirm a successful connection
